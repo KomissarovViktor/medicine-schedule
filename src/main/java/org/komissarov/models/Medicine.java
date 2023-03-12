379d5eaa708a7129cd0ea19b2e7c5a688b641ac8
KomissarovViktor <vitya.komissarov15@gmail.com>
2023-03-12 22:38:47 +0200

    Editted Medicine.java

--- src/main/java/org/komissarov/models/Medicine.java
+++ src/main/java/org/komissarov/models/Medicine.java
@@ -2,7 +2,9 @@ package org.komissarov.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.komissarov.ConsumeState;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 
@@ -10,10 +12,21 @@ import java.util.Date;
 @Getter
 public class Medicine {
     private String name;
-    private Date startDate;
+    private LocalDate startDate;
     private int dosagePerOneTake;
     private int takesPerDay;
     private LocalTime time;
-    private int durationDays;
+    private LocalDate endDate;
     private int availableQuantity;
+    private ConsumeState state = ConsumeState.PLANNED;
+
+    public Medicine(String name, LocalDate startDate, int dosagePerOneTake, int takesPerDay, LocalTime time, LocalDate endDate, int availableQuantity) {
+        this.name = name;
+        this.startDate = startDate;
+        this.dosagePerOneTake = dosagePerOneTake;
+        this.takesPerDay = takesPerDay;
+        this.time = time;
+        this.endDate = endDate;
+        this.availableQuantity = availableQuantity;
+    }
 }
