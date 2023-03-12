31bdc95bd94fd903a5992b6c9a4e1c46b0176130
KomissarovViktor <vitya.komissarov15@gmail.com>
2023-03-12 22:40:45 +0200

    Editted Main class

--- src/main/java/org/komissarov/Main.java
+++ src/main/java/org/komissarov/Main.java
@@ -1,7 +1,15 @@
 package org.komissarov;
 
+import org.komissarov.models.Medicine;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Menu menu = new Menu();
+        menu.showMainMenu();
     }
 }
